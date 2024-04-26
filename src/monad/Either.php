1dd4e8fa33dfc670c32dc15866621a9e3fd46e2c
lixianjun <lixianjun_dousong@163.com>
2024-04-26 13:30:29 +0800

    first commit

---
+++ src/monad/Either.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace purephp\functional\maonad;
+
+use purephp\functional\maonad\contract\Either as ContractEither;
+
+class Either implements ContractEither
+{
+    protected $value;
+
+    protected $leftValue = null;
+    protected $rightValue = null;
+
+    protected $isSuccess;
+
+    private function __construct($value, bool $isSuccess)
+    {
+        if ($isSuccess) {
+            $this->rightValue = $value;
+        } else {
+            $this->leftValue = $value;
+        }
+
+        $this->isSuccess = $isSuccess;
+    }
+
+    // 创建一个成功的 Either 实例
+    public static function of($value): self
+    {
+        return new static($value, true);
+    }
+
+    // 创建一个失败的 Either 实例
+    public static function left($error): self
+    {
+        return new static($error, false);
+    }
+
+    public function map(): self
+    {
+        return $this;
+    }
+
+    public function isRight(): bool
+    {
+        return $this->isSuccess;
+    }
+
+    public function isLeft(): bool
+    {
+        return !$this->isSuccess;
+    }
+
+    public function getRight()
+    {
+        if (!$this->isRight()) {
+            throw new \RuntimeException('Attempted to get the right value of a Left Either');
+        }
+        return $this->rightValue;
+    }
+
+    public function getLeft()
+    {
+        if (!$this->isLeft()) {
+            throw new \RuntimeException('Attempted to get the left value of a Right Either');
+        }
+        return $this->leftValue;
+    }
+
+    // Bind operation for chaining with Right values
+    public function bind(callable $function): self
+    {
+        if ($this->isRight()) {
+            return $function($this->rightValue);
+        }
+        return $this;
+    }
+
+    // Then operation for performing side effects with Right values
+    public function then(callable $function): self
+    {
+        if ($this->isRight()) {
+            $function($this->rightValue);
+        }
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->isSuccess ? 'Right(' . $this->value . ')' : 'Left(' . $this->value . ')';
+    }
+
+    public static function assertTrue(callable $fn, $data)
+    {
+        if ($fn($data) === true) {
+            return static::of($data);
+        } else {
+            return static::left($data);
+        }
+    }
+}
---
+++ src/monad/contract/Either.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace purephp\functional\maonad\contract;
+
+use Stringable;
+
+interface Either extends Stringable
+{
+
+    // 创建一个成功的 Either 实例
+    public static function of($value): Either;
+
+    // 创建一个失败的 Either 实例
+    public static function left($error): Either;
+
+    public function map(): Either;
+
+    public function isRight(): bool;
+
+    public function isLeft(): bool;
+
+    public function getRight();
+
+    public function getLeft();
+
+    public function bind(callable $function): Either;
+
+    public function then(callable $function): Either;
+
+    public static function assertTrue(callable $fn, $data);
+}
